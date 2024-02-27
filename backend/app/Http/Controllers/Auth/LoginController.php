53295eca0f4d5c00a66e279878f0bde2ac84f880
saihtoolwin <saihtoolwin66@gmail.com>
2024-02-27 22:52:55 +0630

    permission table seeder still doing

--- backend/app/Http/Controllers/Auth/LoginController.php
+++ backend/app/Http/Controllers/Auth/LoginController.php
@@ -2,38 +2,52 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Events\AutoLogoutEvent;
+use App\Events\LoginEvent;
+use App\Events\LogoutEvent;
+use Session;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-
+use Illuminate\Http\Request;
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
+ 
     use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
+  
     protected $redirectTo = '/home';
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    protected function authenticated(Request $request, $user)
+    {
+        event(new LoginEvent($user->id,$user->name , $user->email ,null));
+    }
+    
+    public function logout(Request $request)
+    {
+        $user = auth()->user();
+        
+        if ($user) {
+            event(new LogoutEvent($user->id, Session::get('loginId')));
+        }
+
+        $this->guard()->logout();
+        $request->session()->invalidate();
+
+        $sessionLifetimeMinutes = config('session.lifetime');
+
+
+        if ($sessionLifetimeMinutes) {
+            event(new AutoLogoutEvent($user->id, Session::get('loginId')));
+        }else
+        {
+            dd("Error");
+        }
+
+        return $this->loggedOut($request) ?: redirect('/');
     }
 }
--- backend/resources/views/auth/login.blade.php
+++ backend/resources/views/auth/login.blade.php
@@ -1,73 +1,68 @@
 @extends('layouts.app')
-
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <!-- Sign In Start -->
+    <div class="container-fluid">
+        <div class="row h-100 align-items-center justify-content-center" style="min-height: 100vh;">
+            <div class="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+                <div class="bg-light rounded p-4 p-sm-5 my-4 mx-3">
+                    <div class="d-flex align-items-center justify-content-between mb-3">
+                        <a href="index.html" class="">
+                            <h3 class="text-primary"><i class="fa fa-hashtag me-2"></i>DASHMIN</h3>
+                        </a>
+                        <h3>Sign In</h3>
+                    </div>
+                    <form action="{{ route('login') }}" method="post">
                         @csrf
+                        <div class="form-floating mb-3">
+                            <label for="floatingInput">Email address</label>
+                            <input type="email" id="floatingInput" placeholder="name@example.com"
+                                class="form-control @error('email') is-invalid @enderror" name="email"
+                                value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
                         </div>
+                        <div class="form-floating mb-4">
+                            <label for="floatingPassword">Password</label>
+                            <input type="password" class="form-control @error('password') is-invalid @enderror"
+                                name="password" required autocomplete="current-password" id="floatingPassword"
+                                placeholder="Password">
+                            @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
                         </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
+                        <div class="d-flex align-items-center justify-content-between mb-4">
+                            <div class="form-check">
+                                <input type="checkbox" class="form-check-input"  name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                <label class="form-check-label"for="remember">{{ __('Remember Me') }}</label>
                             </div>
+                            <a href="">Forgot Password</a>
                         </div>
+                        
+                        {{-- <button type="submit" class="btn btn-primary py-3 w-100 mb-4">Sign In</button> --}}
+                        <button type="submit" class="btn btn-primary py-3 w-100 mb-4">
+                            {{ __('Login') }}
+                        </button>
 
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
+                        @if (Route::has('password.request'))
+                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                                {{ __('Forgot Your Password?') }}
+                            </a>
+                        @endif
+                        <p class="text-center mb-0">Don't have an Account? <a href="">Sign Up</a></p>
                     </form>
                 </div>
+
+
             </div>
         </div>
     </div>
-</div>
+    <!-- Sign In End -->
 @endsection
